32dcf9a9d4e5696604dc87cdf7c2a3dbd566b2e1
Joshua Turgeon <josh@joshturge.dev>
2021-06-06 22:33:06 +1000

    Added better error handling and loading embed messages

--- dhost_test.sh
+++ dhost_test.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	printf "dhost_test: host was not provided\n" >&2
+function err {
+	printf "dhost_test: %s\n" "$1" >&2
 	exit 1
+}
+
+if [ -z "$1" ]; then
+	err "host was not provided"
 else
 	TARGET_HOST="$1"
 fi
 
 if [ -z "$DHOST_WEBHOOK_ID" ]; then
-	printf "dhost_test: discord webhook id was not provided\n" >&2
-	exit 1
+	err "discord webhook id was not provided"
 fi
 
 if [ -z "$DHOST_WEBHOOK_TOKEN" ]; then
-	printf "dhost_test: discord webhook token was not provided\n" >&2
-	exit 1
+	err "discord webhook token was not provided"
 fi
+
+DATA_DIR="data"
+
+if [ ! -d "$DATA_DIR" ]; then
+	err "data directory: no such directory"
+fi
+
+ON_DOWN="$(cat $DATA_DIR/on_down.json)"
+ON_UP="$(cat $DATA_DIR/on_up.json)"
 
 WEBHOOK_URL="https://discord.com/api/webhooks/$DHOST_WEBHOOK_ID/$DHOST_WEBHOOK_TOKEN"
 
@@ -28,25 +39,27 @@ if [ -z "$DHOST_CURL_TIMEOUT" ]; then
 fi
 
 function send_message {
-	curl -X POST -F "content=$1" $WEBHOOK_URL
+	curl -X POST -H "Content-Type: application/json" \
+		--data "$1" $WEBHOOK_URL
 }
 
 is_host_down=false
 
 while true; do
 	if ! curl --silent --max-time $DHOST_CURL_TIMEOUT $TARGET_HOST >/dev/null; then
-		printf "dhost_test: timeout of %s seconds has been reached\n" "$DHOST_CURL_TIMEOUT" >&2
 
 		if ! $is_host_down; then
+			printf "dhost_test: timeout of %s seconds has been reached. Host is down\n" "$DHOST_CURL_TIMEOUT"
 			is_host_down=true
-			send_message "The host ${TARGET_HOST} has gone offline"
+			send_message $ON_DOWN
 		fi
 
 	else
 
 		if $is_host_down; then
+			printf "dhost_test: Host is up\n" "$DHOST_CURL_TIMEOUT"
 			is_host_down=false
-			send_message "The host ${TARGET_HOST} has come back online"
+			send_message $ON_UP
 		fi
 	fi
 
